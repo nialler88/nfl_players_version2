ac9a6d772e0bc40bf2a4990d67492a09a7d32e6b
Niall Harney <NiallHarney@nialls-air.wit.ie>
2017-04-27 15:10:46 +0100

    initial commit

---
+++ config/development.js
@@ -0,0 +1,9 @@
+'use strict';
+// Development specific configuration
+// ==================================
+module.exports = {
+    // MongoDB connection options
+mongo: {
+uri: 'mongodb://root:root@ds127998.mlab.com:27998/web-services'
+}
+};
